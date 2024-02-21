0b066fffb8446753a3858ac69bad88b955b0e3aa
gjb <theboag@gmail.com>
2024-02-21 22:19:05 +0000

    c run

---
+++ C_fixxbuzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void fizzBuzz(int n){
+    for (int i = 1; i <= 100; ++i){
+        if (i%5 == 0 && i%3 == 0)
+        printf("FizzBuzz! \n");
+        else if (i%5==0)
+        printf("Buzz! \n");
+        else if(i%3==0)
+        printf("Fizz! \n");
+        else
+        printf("%d\n", i);
+    }
+}
+
+int main(){
+    int n = 100;
+    fizzBuzz(n);
+    return 0;
+}
