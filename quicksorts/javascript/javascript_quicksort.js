8de9808f08721f09240c2787f327973327d827ae
Diomedes Git <diomedesgit@gmail.com>
2024-03-03 19:02:34 +0000

    javascript quicksort typo fixed

--- quicksorts/javascript/javascript_quicksort.js
+++ quicksorts/javascript/javascript_quicksort.js
@@ -6,13 +6,13 @@ function quicksort(data){
         const pivot = data[0];
         const left = [];
         const right = [];
-        for (left i = 1: i < data.length; i++) {
-            if (data[i] < pivot) {
-                left.push(data[i]);
-            } else {
-                right.push(data[i]);
+        for (let i = 1; i < data.length; i++) {
+                if (data[i] < pivot) {
+                    left.push(data[i]);
+                } else {
+                    right.push(data[i]);
+                }
             }
-    }
     return [...quicksort(left),...quicksort(right)];
-}
+    }
 }
