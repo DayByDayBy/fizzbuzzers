37a1a884dcf96bd586e5dc498914a26ca0f2a29d
Diomedes Git <diomedesgit@gmail.com>
2024-03-09 23:03:35 +0000

    adding a try/catch to bubble

--- sorting/javascript/javascript_bubblesort.js
+++ sorting/javascript/javascript_bubblesort.js
@@ -1,25 +1,31 @@
 // print('\n')    #  not necessary at all, but improves command-line readability when running 
 
 const fs = require('fs');
-const data = fs.readFileSync('../data.txt', 'utf8').split(',').map(Number);
+let data;
+try {
+    data = fs.readFileSync('../data.txt', 'utf8').split(',').map(Number);
+} catch (error) {
+    console.error('error while reading file:', error);
+}
 
 
 function bubblesort(data){
+    
     let n = data.length;
+
     for (let i=0; i<n; i++){
         for (let j=0; j<n-i-1;j++){
             if (data[j] > data[j+1]){
                 let temp = data[j];
                 data[j] = data[j+1];
                 data[j+1] = temp;
-                }
             }
         }
-    return data
+    }
+return data
     
 }
 
 bubblesort(data)
 console.log(bubblesort(data));
 
-// # print(quickSorted[0], "items, sorted in ", quickSorted[1], "seconds:  ", '\n\n', quickSorted[2][:5], "(list truncated for legibility)")
