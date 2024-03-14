23f366364e2004c78c30c2c2c3b02aa02fad747d
Diomedes Git <diomedesgit@gmail.com>
2024-03-14 23:49:41 +0000

    added golfed py

--- fizzbuzzers/python/python_fizzbuzz_callable.py
+++ fizzbuzzers/python/python_fizzbuzz_callable.py
@@ -10,3 +10,6 @@ def fizzbuzzer(n):
     else:
         return str(n)
     
+    
+def alt(n):  
+    return "FizzBuzz" if n % 15 == 0 else "Fizz" if n % 3 == 0 else "Buzz" if n % 5 == 0 else str(n)
