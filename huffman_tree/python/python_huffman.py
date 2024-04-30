80ab99fbd170f58bc1a88d871ca562405750d587
Diomedes Git <diomedesgit@gmail.com>
2024-04-30 21:00:55 +0100

    i seem to have broken it, lol. commiting to save game, but will probably revert to older version and go from there instead

--- huffman_tree/python/python_huffman.py
+++ huffman_tree/python/python_huffman.py
@@ -1,88 +1,39 @@
-
 import heapq
 from collections import Counter
-from typing import Optional
-import json
 
-class TreeNode:
-    def __init__(self, char: Optional[str] = None, freq: int = 0):
-        self.char = char
-        self.freq = freq
-        self.left_node: Optional[TreeNode] = None
-        self.right_node: Optional[TreeNode] = None
+def read_file(filename):
+    with open(filename, 'r') as file:
+        text = file.read()
+    return text
 
-    def __lt__(self, other: "TreeNode") -> bool:
-        return self.freq < other.freq
+def write_file(filename, encoded_text, codes):
+    with open(filename, 'w') as file:
+        file.write(''.join(encoded_text))
+        file.write('\n')
+        for char, code in codes.items():
+            file.write(f'{char}: {code}\n')
 
-def build_frequency_map(text: str) -> Counter[str]:
-    # build frequency map from input text
-    return Counter(text)
+def build_huffman_tree(freq_dict):
+    heap = [[wt, [sym, '']] for sym, wt in freq_dict.items()]
+    heapq.heapify(heap)
+    while len(heap) > 1:
+        lo = heapq.heappop(heap)
+        hi = heapq.heappop(heap)
+        for pair in lo[1:]:
+            pair[1] = '0' + pair[1]
+        for pair in hi[1:]:
+            pair[1] = '1' + pair[1]
+        heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+    return sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
 
-def build_priority_queue(freq_map: Counter[str]) -> list[TreeNode]:
-    # build priority queue of leaf nodes from frequency map
-    return [TreeNode(char, freq) for char, freq in freq_map.items()]
+def huffman_encode(input_filename, output_filename):
+    text = read_file(input_filename)
+    freq_dict = Counter(text)
+    codes = {sym: code for code, sym in build_huffman_tree(freq_dict)}
+    print(codes)
+    encoded_text = [codes[char] for char in text]
+    write_file(output_filename, encoded_text, codes)
 
-
-def merge_nodes(left_node: TreeNode, right_node: TreeNode) -> TreeNode:
-    # merge two nodes into a new node with the sum of their frequencies
-    merged_node = TreeNode(freq=left_node.freq + right_node.freq)
-    merged_node.left_node = left_node
-    merged_node.right_node = right_node
-    
-    return merged_node
-
-def build_huffman_tree(text: str) -> Optional[TreeNode]:
-    # build the huffman tree from the input text, with 'if not' as error catch
-    if not text:
-        return None
-
-    freq_map = build_frequency_map(text)
-    priority_queue = build_priority_queue(freq_map)
-    heapq.heapify(priority_queue)
-
-    while len(priority_queue) > 1:
-        left_node = heapq.heappop(priority_queue)
-        right_node = heapq.heappop(priority_queue)
-        merged_node = merge_nodes(left_node, right_node)
-        heapq.heappush(priority_queue, merged_node)
-
-    return priority_queue[0]
-
-def tree_to_dict(node):
-    if node is None:
-        return None
-    return {
-        "char": node.char,
-        "freq": node.freq,
-        "left": tree_to_dict(node.left_node),
-        "right": tree_to_dict(node.right_node)
-    }
-    
-    
-def encode_text(text, huffman_tree):
-    encoding_map = {}
-    def traverse(node, code=""):
-        if node['char'] is not None:
-            encoding_map[node['char']] = code
-            return 
-        traverse(node['left'], code + '0')
-        traverse(node['right'], code + '1')
-    traverse(huffman_tree)
-    encoded_text = ''.join(encoding_map[char] for char in text)
-    return encoded_text
-
-
-
-with open('../lorem.txt', 'r') as file:
-    text = file.read()
-huffman_tree_root = build_huffman_tree(text)
-
-tree_dictionary = tree_to_dict(huffman_tree_root)
-
-with open("huffman_tree.json", "w") as file:
-    json.dump(tree_dictionary, file, indent=4)
-encoded_text = encode_text(text, tree_dictionary)
-
-with open ("encoded_text.txt", "w") as file:
-    file.write(encoded_text)
-
+input_filename = '../lorem.txt'
+output_filename = 'encoded.txt'
+huffman_encode(input_filename, output_filename)
